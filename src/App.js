dfe4103ed8a1c06c849a1139af6974937bb0d906
shanugupta7999 <shanugupta8269@gmail.com>
2024-08-26 23:20:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
+function App() 
+{
 
-function App() {
+  const [joke, setJoke] = useState('');
+
+  const getJoke = () => {
+    Axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then(
+      response => {
+        console.log(response);
+        setJoke(response.data.setup + ' ... ' + response.data.punchline);
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-blue-50 shadow-md p-2">
+      <section className='border p-5 rounded-md bg-blue-300 text-center h-96 w-96 flex flex-col justify-center items-center gap-10'>
+            <div>
+              <h1 className="font-semibold text-white text-xl">Joke Generator Using React And Joke API</h1>
+            </div>
+            <div className="flex items-center justify-center max-h-40 max-w-90 rounded-md p-1 bg-white">
+              <p>{joke}</p>
+            </div>
+            <button className='border border-black rounded-md px-5 py-1  bg-blue-500 text-white hover:bg-blue-600 transition 0.5s' onClick={getJoke}>Click To Generate Joke</button>
+      </section>
+      
     </div>
   );
 }
